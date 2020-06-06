cec8ed11718ffaa533964043c9fa5d61e39ecc69
david <david.fu@siloon.com>
2020-06-06 19:43:05 +0800

    add docker windows config

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -40,8 +40,7 @@ namespace AuthService
                     var endpointConfiguration = new EndpointConfiguration("authservice");
                     //var transport = endpointConfiguration.UseTransport<LearningTransport>();
                     var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
-                    transport.ConnectionString("host=rabbitmq");
-                    //transport.ConnectionString("host=192.168.99.100");
+                    transport.ConnectionString(hostBuilderContext.Configuration.GetConnectionString("RabbitMQ"));
                     transport.UseConventionalRoutingTopology();
                     endpointConfiguration.EnableInstallers();
 
